67b7301d159d2f1e9e7a0c54578c469922fd7ad0
SIMAJCHEL <SIMAJCHEL@intechS1.fr>
2018-05-02 10:26:20 +0200

    détermine valeur des cases

--- t.js
+++ t.js
@@ -7,6 +7,8 @@ var nb_lig;
 var nb_col;
 var prob;
 var p;
+var x;
+var y;
 
 nb_lig = 10;
 nb_col = 10;
@@ -17,7 +19,7 @@ for(l=0; l<nb_lig; l++) {
 	grille[l] = [];
 	for(c=0; c<nb_col; c++) {
 		//console.log("on remplit la case de ligne " + l + "et de colonne " + c);
-		grille[l][c] = {"d": false, "show": false};
+		grille[l][c] = {"d": false, "show": false, "v" : 0};
 		p = Math.floor(Math.random() * 10);
 		if(p === 0) {
 			grille[l][c].b = true;
@@ -43,4 +45,56 @@ for(l=0; l<nb_lig; l++) {
 
 // remplir la propriete "v"
 
+for(l=0; l<nb_lig; l++) {
+	for(c=0; c<nb_col; c++) {
+		if (grille[l][c].b === true) {
+			for (x=-1;x<2;x++) {
+				for (y=-1; y<2;y++) {
+					if(x !== 0 || y !== 0) {
+						if (l+x>=0 && l+x<nb_lig && c+y>=0 && c+y<nb_col) {
+							grille[l+x][c+y].v = grille[l+x][c+y].v +1 
+						}
+					}
+				}	
+			}
+			//	if (l-1 < -1 && l+1 <= nb_lig) {
+			//		if ( c-1 < -1 && l+1 <= nb_col) {
+		}
+	}
+}
 
+
+console.log(grille)
+
+for(l=0; l<nb_lig; l++) {
+	for(c=0; c<nb_col; c++) {
+		if (grille[l][c].b === true) {
+			process.stdout.write("B ");
+		} else {
+			process.stdout.write(". ");
+		}
+	}
+	console.log("");
+}
+
+console.log("");
+
+for(l=0; l<nb_lig; l++) {
+	for(c=0; c<nb_col; c++) {
+		process.stdout.write(grille[l][c].v + " ");
+	}
+	console.log("");
+}
+
+console.log("");
+
+for(l=0; l<nb_lig; l++) {
+	for(c=0; c<nb_col; c++) {
+		if (grille[l][c].b === true) {
+			process.stdout.write("B ");
+		} else {
+			process.stdout.write(grille[l][c].v + " ");
+		}
+	}
+	console.log("");
+}
