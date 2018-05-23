4c1fb8316379f7975d76611439465f1e17b0a653
ELEVE <ELEVE@intechS1.fr>
2018-05-23 15:44:44 +0200

    tropicana

---
+++ req_retour.js
@@ -0,0 +1,24 @@
+// Requete quitter pour le mode facile 
+
+
+"use strict"
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req ,res ,query) {
+var page;
+var marqueur = {};
+
+	page = fs.readFileSync("modele_accueil_membre.html","utf-8");
+
+	marqueur.pseudo = query.pseudo;
+	page = page.supplant(marqueur);
+
+
+	res.writeHead(200,{"Content-Type": "text/html"});
+	res.write(page);
+	res.end();
+};
+
+module.exports = trait;
